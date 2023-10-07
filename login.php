d1e1aa9a1f0ff0e77c650c238a7c087a1ceed7b9
Ttmach888 <145077437+Ttmach888@users.noreply.github.com>
2023-10-07 12:58:58 -0600

    Update login.php

--- login.php
+++ login.php
@@ -5,17 +5,27 @@ include_once 'navbar.php';
 ?>
 
 	<!-- Main container of page -->
-	<div class="container-fluid">
-		<div class="row">
-			<div class="main-view landingPadding">
-				<!-- Login in form with fields for username and password. Submit button via POST -->
-                <!-- Link to create a new account -->
-			</div>
-		</div>
-	</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="main-view landingPadding">
+            <!-- Login form with fields for username and password -->
+            <form action="your_login_script.php" method="POST">
+                <div class="form-group">
+                    <label for="username">Username:</label>
+                    <input type="text" class="form-control" id="username" name="username" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+            <!-- Link to create a new account -->
+            <p>Don't have an account? <a href="register.php">Create a new account</a></p>
+        </div>
+    </div>
+</div>
 	<!-- End main container -->
-
-
 <?php
 include_once 'footer.php';
 ?>
