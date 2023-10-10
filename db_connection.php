d6bd01d93c385f5769f73f651f85c94236379821
Garret-Thompson <147113699+Garret-Thompson@users.noreply.github.com>
2023-10-09 21:20:38 -0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,15 +1,14 @@
 <?php
-function OpenCon()
-{
-$dbhost = "localhost";
-$dbuser = "test_user";
-$dbpass = "1234";
-$dbname = "test";
-$conn = new mysqli($dbhost, $dbuser, $dbpass,$dbname) or die("Connect failed: %s\n". $conn -> error);
-return $conn;
+
+$DB_HOST = 'localhost'; // Change to your MySQL host name (often 'localhost' is enough)
+$DB_USER = 'gtthompson84'; // Change to your MySQL user name
+$DB_PASS = 'XXXXXXX'; // Change to your MySQL password
+$DB_NAME = 'login'; // Change to your database name
+
+// Creating the connection
+$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+
+// Checking the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-function CloseCon($conn)
-{
-$conn -> close();
-}
-?>
