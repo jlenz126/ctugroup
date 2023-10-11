f8a7daf69278a4d521ea02319cc98c0229ecaf1f
Jacob <jlenz126@gmail.com>
2023-10-11 07:48:00 -0500

    Commented out the reviews link in navbar

--- navbar.php
+++ navbar.php
@@ -14,9 +14,11 @@
 		  	<li class="nav-item">
 	          <a class="nav-link active" aria-current="page" href="login.php">Login</a>
 	        </li>
-		 	<li class="nav-item">
+		 	<!-- Feature to be added later
+			<li class="nav-item">
 	          <a class="nav-link active" aria-current="page" href="reviews.php">Reviews</a>
 	        </li>
+			-->
 			<li class="nav-item">
 	          <a class="nav-link active" aria-current="page" href="contact.php">Contact Us</a>
 	        </li>
