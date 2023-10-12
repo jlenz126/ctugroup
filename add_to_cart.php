0108b7c91ed4cd85790a6a8bad73323694203a64
Jacob <jlenz126@gmail.com>
2023-10-12 16:50:31 -0500

    add logic for drinks

--- add_to_cart.php
+++ add_to_cart.php
@@ -5,14 +5,15 @@ include_once 'navbar.php';
 include_once 'db_connection.php';
 $itemID = $_POST['itemID'];
 
-if(!isset($_POST['itemName']) && !isset($_POST['itemID'])){
+if($_POST['categoryID'] == 1){
+    //code to add appetizer straight to cart
+    //redirect back to menu
     header("Location: menu.php");
 }
 
-if($_POST['itemID'] == 1){
-    //code to add appetizer straight to cart
-    //redirect back to menu
-}
+//Code to process pizza to cart
+
+//Code to process kids meal to cart
 ?>
 
 	<!-- Main container of page -->
@@ -22,46 +23,96 @@ if($_POST['itemID'] == 1){
                 <?php
                 $conn = OpenCon();
 
-                switch ($_POST['categoryID']){
-                    case 2:
-                        $sql_toppings = "SELECT id, topping_name, premium_topping FROM topping";
-                        $result_toppings = $conn->query($sql_toppings);
-                        $sql_default_toppings = "SELECT default_topping FROM item WHERE id = $itemID";
-                        $result_default_toppings = $conn->query($sql_default_toppings);
-                        $toppings_string = $result_default_toppings->fetch_row()[0];
-                        
-                        echo '<form method="post" action="add_to_cart.php">';
-                            if($result_toppings->num_rows >0){
-                                while($row = $result_toppings->fetch_assoc()){
-                                    $topping_name_display = ucwords(strtolower($row['topping_name']));
-                                    $checked = null;
-                                    if(str_contains($toppings_string, $row['topping_name'])){
-                                        $checked = 'checked=""';
-                                    }
+                if(isset($_POST['categoryID'])){
+                    switch ($_POST['categoryID']){
+                        case 2:
+                            $sql_toppings = "SELECT id, topping_name, premium_topping FROM topping";
+                            $result_toppings = $conn->query($sql_toppings);
+                            $sql_default_toppings = "SELECT default_topping FROM item WHERE id = $itemID";
+                            $result_default_toppings = $conn->query($sql_default_toppings);
+                            $toppings_string = $result_default_toppings->fetch_row()[0];
 
-                                    echo '<div class="form-check">';
-                                        echo '<input class="form-check-input" type="checkbox" name="flexcheck" id="'. $row['topping_name'] .'" value="'. $row['topping_name'] .'" '. $checked .'>';
-                                        echo '<label class="form-check-label" for="'. $row['topping_name'] .'">';
-                                        echo $topping_name_display;
-                                        echo '</label>';
-                                    echo '</div>';
+                            echo '<form method="post" action="add_to_cart.php">';
+                                echo '<select class="form-select form-select-lg mb-3" aria-label="Large select example" name="size">';
+                                    echo '<option value="small" selected>Small</option>';
+                                    echo '<option value="medium">Medium +$2.00</option>';
+                                    echo '<option value="large">Large +$4.00</option>';
+                                echo '</select>';
+                                if($result_toppings->num_rows >0){
+                                    while($row = $result_toppings->fetch_assoc()){
+                                        $topping_name_display = ucwords(strtolower($row['topping_name']));
+                                        $checked = null;
+                                        if(str_contains($toppings_string, $row['topping_name'])){
+                                            $checked = 'checked=""';
+                                        }
+
+                                        echo '<div class="form-check">';
+                                            echo '<input class="form-check-input" type="checkbox" name="flexcheck" id="'. $row['topping_name'] .'" value="'. $row['topping_name'] .'" '. $checked .'>';
+                                            echo '<label class="form-check-label" for="'. $row['topping_name'] .'">';
+                                            echo $topping_name_display;
+                                            echo '</label>';
+                                        echo '</div>';
+                                    }
                                 }
-                            }
-                        echo '</form>';
-                        break;
-                    case 3:
-                        echo "kid meal";
-                        break;
-                    case 4:
-                        echo "combo";
-                        break;
-                    case 5:
-                        echo "drink";
-                        break;
-                    default:
-                        echo "default";
+                                echo '<div class="text-center">';
+                                echo '<button type="submit" class="btn btn-light btn-lg">Confirm</button>'; 
+                                echo '</div>';
+                                echo '<input type="hidden" id="processID" name="processID" value="'. $_POST['itemID'] .'">';
+                            echo '</form>';
+                            break;
+                        case 3:
+                            $sql_drinks = "SELECT item_name FROM item WHERE category_id = 5";
+                            $result_drinks = $conn->query($sql_drinks);
+                            
+                            echo '<form method="post" action="add_to_cart.php">';
+                                echo '<select id="drink-select" class="form-select form-select-lg mb-3" aria-label="Large select example" name="drink">';
+                                echo '<option></option>';
+                                    if($result_drinks->num_rows > 0){
+                                        while($row = $result_drinks->fetch_assoc()){
+                                            $drink_name_display = ucwords(strtolower($row['item_name']));
+                                            echo '<option value="'. $row['item_name'] .'">'. $drink_name_display .'</option>';
+                                        }
+                                    }
+                                echo '</select>';
+                            echo '<div class="text-center">';
+                            echo '<button type="submit" class="btn btn-light btn-lg">Confirm</button>'; 
+                            echo '</div>';
+                            echo '<input type="hidden" id="processID" name="processID" value="'. $_POST['itemID'] .'">';
+                            echo '</form>';
+                            break;
+                        case 4:
+                            echo "combo";
+                            break;
+                        case 5:
+                            $sql_drinks = "SELECT item_name FROM item WHERE category_id = 5";
+                            $result_drinks = $conn->query($sql_drinks);
+                            
+                            echo '<form method="post" action="add_to_cart.php">';
+                                echo '<select class="form-select form-select-lg mb-3" aria-label="Large select example" name="size">';
+                                    echo '<option value="can" selected>20oz Bottle</option>';
+                                    echo '<option value="2liter">2 Liter +$3.00</option>';
+                                echo '</select>';
+                                echo '<select id="drink-select" class="form-select form-select-lg mb-3" aria-label="Large select example" name="drink">';
+                                echo '<option></option>';
+                                    if($result_drinks->num_rows > 0){
+                                        while($row = $result_drinks->fetch_assoc()){
+                                            $drink_name_display = ucwords(strtolower($row['item_name']));
+                                            echo '<option value="'. $row['item_name'] .'">'. $drink_name_display .'</option>';
+                                        }
+                                    }
+                                echo '</select>';
+                            echo '<div class="text-center">';
+                            echo '<button type="submit" class="btn btn-light btn-lg">Confirm</button>'; 
+                            echo '</div>';
+                            echo '<input type="hidden" id="processID" name="processID" value="'. $_POST['itemID'] .'">';
+                            echo '</form>';
+                            break;
+                        default:
+                            echo "default";
+                    }
                 }
                 ?>
+
 			</div>
 		</div>
 	</div>
