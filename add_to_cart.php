a4975f9bbb4efe89ddf8e4b42f351d4fc6ca7694
Jacob <jlenz126@gmail.com>
2023-10-13 10:41:35 -0500

    drink add to cart logic and removed drink type choice

--- add_to_cart.php
+++ add_to_cart.php
@@ -39,13 +39,33 @@ if($_POST['processCategory'] == 3){
     header("Location: menu.php");
 }
 //Code to process combo to cart
-if($_POST['processCategory'] == 4){
-    $_SESSION['addedToCartMessage']='combo added';
-    header("Location: menu.php");
-}
-//Code to process drink to cart
-if($_POST['processCategory'] == 5){
-    $_SESSION['addedToCartMessage']='drink added';
+// if($_POST['processCategory'] == 4){
+//     $_SESSION['addedToCartMessage']='combo added';
+//     header("Location: menu.php");
+// }
+
+if(isset($_POST['processCategory']) == 5){
+    $drinkID = $_POST['drink'];
+    $drinkSize = $_POST['size'];
+    $sql_add_drink = "INSERT INTO `order_item` (`id`, `order_id`, `item_id`, `quantity`, `drink_size`, `drink_type`) VALUES (NULL, $orderID, $drinkID, '1', '$drinkSize', NULL);";
+
+    if($conn->query($sql_add_drink) === TRUE){
+        $sql_old_total = "SELECT `order_total` FROM `order` WHERE id=$orderID;";
+        $result_old_total = $conn->query($sql_old_total);
+        $old_total = $result_old_total->fetch_row()[0];
+
+        if($drinkSize == "2liter"){
+            $new_total = $old_total + 5;
+        } else {
+            $new_total = $old_total + 2;
+        }
+        $sql_new_total = "UPDATE `order` SET `order_total` = $new_total WHERE `order`.`id` = $orderID;";
+        $conn->query($sql_new_total);
+        $_SESSION['addedToCartMessage']='drink added type: '. $_POST['drink'] . " size " . $_POST['size'];
+    } else {
+        $_SESSION['addedToCartMessage']= "Failed to add drink";
+    }
+    
     header("Location: menu.php");
 }
 ?>
@@ -95,7 +115,7 @@ if($_POST['processCategory'] == 5){
                             echo '</form>';
                             break;
                         case 3:
-                            $sql_drinks = "SELECT item_name FROM item WHERE category_id = 5";
+                            $sql_drinks = "SELECT id, item_name FROM item WHERE category_id = 5";
                             $result_drinks = $conn->query($sql_drinks);
                             
                             echo '<form method="post" action="add_to_cart.php">';
@@ -104,41 +124,30 @@ if($_POST['processCategory'] == 5){
                                     if($result_drinks->num_rows > 0){
                                         while($row = $result_drinks->fetch_assoc()){
                                             $drink_name_display = ucwords(strtolower($row['item_name']));
-                                            echo '<option value="'. $row['item_name'] .'">'. $drink_name_display .'</option>';
+                                            echo '<option value="'. $row['id'] .'">'. $drink_name_display .'</option>';
                                         }
                                     }
                                 echo '</select>';
                             echo '<div class="text-center">';
                             echo '<button type="submit" class="btn btn-light btn-lg">Confirm</button>'; 
                             echo '</div>';
-                            echo '<input type="hidden" id="processID" name="processID" value="'. $_POST['itemID'] .'">';
+                            echo '<input type="hidden" id="processCategory" name="processCategory" value="'. $_POST['categoryID'] .'">';
                             echo '</form>';
                             break;
                         case 4:
                             echo "combo";
                             break;
                         case 5:
-                            $sql_drinks = "SELECT item_name FROM item WHERE category_id = 5";
-                            $result_drinks = $conn->query($sql_drinks);
-                            
                             echo '<form method="post" action="add_to_cart.php">';
                                 echo '<select class="form-select form-select-lg mb-3" aria-label="Large select example" name="size">';
-                                    echo '<option value="can" selected>20oz Bottle</option>';
+                                    echo '<option value="bottle" selected>20oz Bottle</option>';
                                     echo '<option value="2liter">2 Liter +$3.00</option>';
-                                echo '</select>';
-                                echo '<select id="drink-select" class="form-select form-select-lg mb-3" aria-label="Large select example" name="drink">';
-                                echo '<option></option>';
-                                    if($result_drinks->num_rows > 0){
-                                        while($row = $result_drinks->fetch_assoc()){
-                                            $drink_name_display = ucwords(strtolower($row['item_name']));
-                                            echo '<option value="'. $row['item_name'] .'">'. $drink_name_display .'</option>';
-                                        }
-                                    }
                                 echo '</select>';
                             echo '<div class="text-center">';
                             echo '<button type="submit" class="btn btn-light btn-lg">Confirm</button>'; 
                             echo '</div>';
-                            echo '<input type="hidden" id="processID" name="processID" value="'. $_POST['itemID'] .'">';
+                            echo '<input type="hidden" id="processCategory" name="processCategory" value="'. $_POST['categoryID'] .'">';
+                            echo '<input type="hidden" id="drink" name="drink" value="'. $_POST['itemID'] .'">';
                             echo '</form>';
                             break;
                         default:
