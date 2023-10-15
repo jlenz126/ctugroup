2d29f4e7b9cf3c476c207177db88507bb48db945
Ttmach888 <145077437+Ttmach888@users.noreply.github.com>
2023-10-15 01:30:31 -0600

    Added parts on making the review page.
    I don't know if these will be issues due to the facts that each html script is for a different "page" in reviews.

--- reviews.php
+++ reviews.php
@@ -18,3 +18,61 @@ include_once 'navbar.php';
 <?php
 include_once 'footer.php';
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Restaurant Reviews</title>
+</head>
+<body>
+    <h1>Restaurant Reviews</h1>
+    
+    <!-- Display existing reviews here -->
+    <?php
+    // Replace this with code to fetch and display existing reviews from a database
+    echo "Existing Review 1: 5 stars - Excellent service, highly recommended!";
+    echo "Existing Review 2: 4 stars - Good food, but the service could improve.";
+    // ...
+    ?>
+
+    <!-- Button to take customers to the review form -->
+    <a href="review_form.php">Leave a Review</a>
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Leave a Review</title>
+</head>
+<body>
+    <h1>Leave a Review</h1>
+    
+    <form action="submit_review.php" method="post">
+        <label for="name">Your Name:</label>
+        <input type="text" id="name" name="name" required>
+
+        <label for="rating">Rating (1-5):</label>
+        <input type="number" id="rating" name="rating" min="1" max="5" required>
+
+        <label for="review">Review:</label>
+        <textarea id="review" name="review" rows="4" required></textarea>
+
+        <input type="submit" value="Submit Review">
+    </form>
+</body>
+</html>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $rating = $_POST["rating"];
+    $reviewText = $_POST["review"];
+
+    // Save the review to a database (you need to implement this part)
+
+    // Redirect back to the initial reviews page
+    header("Location: reviews.php");
+    exit;
+}
+?>
