e4793812547177fedc3e7639daca0933861d60d3
Jacob <jlenz126@gmail.com>
2023-10-17 08:41:05 -0500

    created category select box

--- manage_item.php
+++ manage_item.php
@@ -4,13 +4,34 @@ include_once 'header.php';
 include_once 'navbar.php';
 include_once 'db_connection.php';
 $conn = OpenCon();
+
+$sql_categories = "SELECT id, category_name FROM category WHERE 1 ORDER BY display_order";
+$result_categories = $conn->query($sql_categories);
 ?>
 
 	<!-- Main container of page -->
 	<div class="container-fluid">
 		<div class="row">
 			<div class="main-view landingPadding">
-				<!-- Content here -->
+				<?php
+                    echo '<form method="post" action="add_to_cart.php">';
+                        echo '<select id="drink-select" class="form-select form-select-lg mb-3" aria-label="Large select example" name="drink">';
+                        echo '<option></option>';
+                            if($result_categories->num_rows > 0){
+                                while($row = $result_categories->fetch_assoc()){
+                                    $category_name_display = ucwords(strtolower($row['category_name']));
+                                    echo '<option value="'. $row['category_name'] .'">'. $category_name_display .'</option>';
+                                }
+                            }
+                        echo '</select>';
+                    echo '<div class="text-center">';
+                    echo '<button type="submit" class="btn btn-light btn-lg">Confirm</button>'; 
+                    echo '</div>';
+                    // echo '<input type="hidden" id="processCategory" name="processCategory" value="'. $_POST['categoryID'] .'">';
+                    // echo '<input type="hidden" id="itemPrice" name="itemPrice" value="'. $_POST['itemPrice'] .'">';
+                    // echo '<input type="hidden" id="processID" name="processID" value="'. $_POST['itemID'] .'">';
+                    echo '</form>';
+                ?>
 			</div>
 		</div>
 	</div>
