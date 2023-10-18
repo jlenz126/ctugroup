95253b6373421f6e37f8cf4904d3c32eefad6a3a
Ttmach888 <145077437+Ttmach888@users.noreply.github.com>
2023-10-17 19:35:59 -0600

    reviews.php
    Updated changes to make the script connect to the database via sql. Took about 1hr

--- reviews.php
+++ reviews.php
@@ -26,19 +26,32 @@ include_once 'footer.php';
 </head>
 <body>
     <h1>Restaurant Reviews</h1>
-    
+
     <!-- Display existing reviews here -->
     <?php
-    // Replace this with code to fetch and display existing reviews from a database
-    echo "Existing Review 1: 5 stars - Excellent service, highly recommended!";
-    echo "Existing Review 2: 4 stars - Good food, but the service could improve.";
-    // ...
+   
+    try {
+        $pdo = new PDO('mysql:host=localhost;dbname=your_database_name', 'your_username', 'your_password');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $query = "SELECT * FROM reviews";
+        $reviews = $pdo->query($query);
+
+        foreach ($reviews as $row) {
+            echo "Review ID: " . $row['id'] . "<br>";
+            echo "Rating: " . $row['rating'] . " stars<br>";
+            echo "Review: " . $row['reviewText'] . "<br><br>";
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
     ?>
 
     <!-- Button to take customers to the review form -->
     <a href="review_form.php">Leave a Review</a>
 </body>
 </html>
+
 
 <!DOCTYPE html>
 <html>
@@ -47,7 +60,7 @@ include_once 'footer.php';
 </head>
 <body>
     <h1>Leave a Review</h1>
-    
+
     <form action="submit_review.php" method="post">
         <label for="name">Your Name:</label>
         <input type="text" id="name" name="name" required>
@@ -63,16 +76,28 @@ include_once 'footer.php';
 </body>
 </html>
 
+
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $rating = $_POST["rating"];
     $reviewText = $_POST["review"];
 
-    // Save the review to a database (you need to implement this part)
+    try {
+        $pdo = new PDO('mysql:host=localhost;dbname=your_database_name', 'your_username', 'your_password');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Redirect back to the initial reviews page
-    header("Location: reviews.php");
+        $query = "INSERT INTO reviews (name, rating, reviewText) VALUES (?, ?, ?)";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$name, $rating, $reviewText]);
+
+        header("Location: reviews.php");
+        exit;
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+} else {
+    header("Location: review_form.php");
     exit;
 }
-?>
+
