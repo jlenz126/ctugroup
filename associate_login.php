d4d534eb07eb6f7a47ee09fe54be9df7cf078276
Risa <risa.luthor@gmail.com>
2023-10-20 00:47:39 -0500

    enter the commit message for your changes. Lines starting

--- associate_login.php
+++ associate_login.php
@@ -1,50 +1,22 @@
 <?php
-include_once 'session.php';
-include_once 'header.php';
-include_once 'navbar.php';
-?>
+// Check if form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Define your credentials here (replace 'your_username' and 'your_password' with actual values)
+    $valid_username = 'your_username';
+    $valid_password = 'your_password';
 
-<style>
-    
-    body {
-        padding-top: 275px; /*  navbar's height */
-    }
-    .main-view {
-        max-width: 600px;
-        margin: 0 auto;
-        padding: 15px;  /* Spacing */
-    }
-    .form-element,
-    .form-button,
-    .register-link {
-        margin-bottom: 15px;
-    }
-</style>
+    // Get the posted username and password
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-<!-- Main container of page -->
-<div class="container-fluid">
-    <div class="row">
-        <div class="main-view landingPadding">
-
-            <!-- Login Form -->
-            <form action="login_processor.php" method="POST">
-                <div class="form-element">
-                    <label for="username">AssociateID:</label>
-                    <input type="text" id="username" name="username" required class="form-control">
-                </div>
-                <div class="form-element">
-                    <label for="password">Password:</label>
-                    <input type="password" id="password" name="password" required class="form-control">
-                </div>
-                <div class="form-button text-center">
-                    <input type="submit" value="Login" class="btn btn-light btn-lg">
-					<input type="reset" value="Clear" class="btn btn-light btn-lg">
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-<!-- End main container -->
-<?php
-include_once 'footer.php';
+    // Check if the credentials are valid
+    if ($username === $valid_username && $password === $valid_password) {
+        // Redirect to the associate dashboard or any other page after successful login
+        header("Location: associate_dashboard.php");
+        exit();
+    } else {
+        // Show an error message for invalid credentials
+        echo "Invalid username or password. Please try again.";
+    }
+}
 ?>
