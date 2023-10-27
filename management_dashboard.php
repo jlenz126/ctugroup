3f1a81d67211a6daa78ac61dcec446de65c8c5be
Jacob <jlenz126@gmail.com>
2023-10-27 08:32:00 -0500

    activated active orders

--- management_dashboard.php
+++ management_dashboard.php
@@ -23,7 +23,7 @@ if(isset($_POST['manageActivity'])){
 
     switch($_POST['manageActivity']){
         case 'activeOrders':
-            $caseID = $_POST['manageActivity'];
+            header('Location: active_orders.php');
             break;
         case 'manageItem':
             header('Location: manage_item.php');
